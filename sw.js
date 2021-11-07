befd661dc1972838459244d76fa3d488b6e13e95
Erickiller <bizarroluna@gmail.com>
2021-11-06 21:06:25 -0600

    se cambio el sw

--- sw.js
+++ sw.js
@@ -23,11 +23,11 @@ const APP_SHELL = [
 ];
 
 const APP_SHELL_INMU = [
-    '/js/libs/jquery.js',
+    'js/libs/jquery.js',
     'https://fonts.googleapis.com/css?family=Quicksand:300,400',
     'https://fonts.googleapis.com/css?family=Lato:400,300',
     //'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
-    '/css/animate.css'
+    'css/animate.css'
 ];
 
 
